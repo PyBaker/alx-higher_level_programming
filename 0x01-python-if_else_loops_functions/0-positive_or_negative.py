47195430841784707b939b0ff6fbb338b865a520
PyBaker <freddygekko3@gmail.com>
2021-10-06 09:21:45 -0700

    to check if number is positive or negative

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/binpython3
+import random
+number = random.randint(-10, 10)
+if number is < 0:
+    print("{0:d} is negative".format(number))
+elif number is = 0:
+    print("{0:d} is zero".format(number))
+elif number is > 0:
+    print("{0:d} is positive".format(number))
