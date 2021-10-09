cb9f3b4c52da56868fd07d5f31a19411f00359d0
PyBaker <freddygekko3@gmail.com>
2021-10-09 07:24:37 -0700

    hotfix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@
 def uppercase(str):
 
     for letter in str:
-        if ord(letter) > 96 and ord(letter) < 122:
+        if ord(letter) > 96 and ord(letter) < 123:
             letter = chr(ord(letter)-32)
         print("{}".format(letter), end='')
     print()
