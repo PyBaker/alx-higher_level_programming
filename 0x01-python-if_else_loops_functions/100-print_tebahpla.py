063e604feb5b5b5dcde6f9c077767ac1ec222f30
PyBaker <freddygekko3@gmail.com>
2021-10-09 11:01:01 -0700

    removed function and turned script into a program

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 
-def uppercase():
-
-    for i in range(ord('z'), ord('a')-1, -1):
-        print("{}".format(chr(i) if i % 2 == 0 else chr(i-32)), end='')
+for i in range(ord('z'), ord('a')-1, -1):
+    print("{}".format(chr(i) if i % 2 == 0 else chr(i-32)), end='')
