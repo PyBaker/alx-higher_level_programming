2c153efffa784a9d055fa066a8ce9e91af9548bf
PyBaker <freddygekko3@gmail.com>
2021-10-16 20:09:11 +0200

    used more spefic exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,14 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except TypeError:
+        except TypeError as te:
+            #print("Pano pa {}".format(te))
+            pass
+        except ValueError as ve:
+            #print("Pano pa two {}".format(ve))
+            pass
+        except IndexError as ve:
+            #print("Pano pa three {}".format(ve))
             pass
     print()
-
+    return count
