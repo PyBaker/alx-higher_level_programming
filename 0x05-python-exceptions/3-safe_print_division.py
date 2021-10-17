2724a19814294fc5af539a2b519a09830d159f13
PyBaker <freddygekko3@gmail.com>
2021-10-17 22:38:31 +0200

    added Type Error to exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,7 +6,7 @@ def safe_print_division(a, b):
 
     try:
         res = (a / b)
-    except ZeroDivisionError:
+    except (ZeroDivisionError, TypeError):
         res = None
     finally:
         print("Inside result: {}".format(res))
