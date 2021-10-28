f335c25df0e1049153486fc097b8cc2da3724003
PyBaker <freddygekko3@gmail.com>
2021-10-28 20:18:36 +0200

    experimented with getters and setter property decorators

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,9 +7,6 @@ class Square:
     Defines a square
 
     """
-    def area(self):
-        return self.__size ** 2
-
     def __init__(self, size=0):
         """
         Initialize a square
@@ -23,3 +20,6 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
+    def area(self):
+        return self.__size ** 2
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -15,26 +15,30 @@ class Square:
         size(int) = size of a square
         """
         self.__size = size
-        #print("i am size {} and i'm varible {}".format(size, self.__size))
 
     @property
     def size(self):
-        #print("this is the getter")
         return self.__size
 
     @size.setter
     def size(self, value):
-        #print ("this is th value {}".format(value))
 
         if type(value) is not int:
-            #print("Hello")
             raise TypeError("size must be an integer")
         elif value < 0:
-            #print("World")
             raise ValueError("size must be >= 0")
         else:
-            #print("i'm done setting value {} to __size {}".format(value, self.__size))
             self.__size = value
 
     def area(self):
         return (self.__size) ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for row in range(self.__size):
+                print()
+                for column in range(self.__size):
+                    print("#", end='')
+            print()
