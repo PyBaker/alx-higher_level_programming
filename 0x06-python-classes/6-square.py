cc3f8df7381f3acb214a804b75504a54cf0cd342
PyBaker <freddygekko3@gmail.com>
2021-10-31 18:24:37 +0200

    code cleanging

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -53,5 +53,5 @@ class Square:
             print()
         else:
             print("\n" * self.__position[1], end="")
-            print('\n'.join([" " * self.__position[0]
-                            + '#' * self.__size] * self.__size))
+            print('\n'.join([" " * self.__position[0] + '#' * self.__size]
+                            * self.__size))
