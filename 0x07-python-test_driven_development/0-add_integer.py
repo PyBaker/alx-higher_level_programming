78233e0a7c165b1f6357e6aca81fc14dffc83081
PyBaker <freddygekko3@gmail.com>
2021-11-03 17:08:35 +0200

    edited doctest file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,12 +4,12 @@
 def add_integer(a, b=98):
     ''' return sum of two numbers '''
     if type(a) is not int:
-        raise(TypeError("a must be an integer")
+        raise TypeError("a must be an integer")
     elif type(b) is not int:
-        raise(TypeError("b must be an integer")
+        raise TypeError("b must be an integer")
     elif type(a) is float:
         a = int(a)
-    elif type(b) is float
+    elif type(b) is float:
         b = int(b)
     else:
         return (a + b)
