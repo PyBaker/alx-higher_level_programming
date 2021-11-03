13db47ffc5fcc4bb96d502322537c12c58440399
PyBaker <freddygekko3@gmail.com>
2021-11-03 15:46:23 +0200

    done with task 1 ( sum of two numbers )

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+''' This module tests adds two numbers and casts floats to int'''
+
+def add_integer(a, b=98):
+    ''' return sum of two numbers '''
+    if type(a) is not int:
+        raise(TypeError("a must be an integer")
+    elif type(b) is not int:
+        raise(TypeError("b must be an integer")
+    elif type(a) is float:
+        a = int(a)
+    elif type(b) is float
+        b = int(b)
+    else:
+        return (a + b)
