1e6f6f4b8270a9a2e7ee87414bf47fca31917bc5
PyBaker <freddygekko3@gmail.com>
2021-11-05 21:05:13 +0200

    Print Full name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+'''
+Module with funtions :
+say_my_name --> prints string with first name and last name
+'''
+
+
+def say_my_name(first_name, last_name=""):
+
+    if isinstance(first_name, str) and isinstance(last_name, str):
+        print("My name is {0:s} {1:s}".format(first_name, last_name))
+    else:
+        raise TypeError("{:s} must be a string"
+                        .format('last_name' if isinstance(first_name, str)
+                            else 'first_name'))
+
+def main():
+    say_my_name(4,'Hello')
+
+
+
+if __name__ == "__main__":
+    main()
