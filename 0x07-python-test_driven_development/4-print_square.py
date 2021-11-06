8647630176db07c6ba537c41fbc4bc6649686989
PyBaker <freddygekko3@gmail.com>
2021-11-06 20:57:39 +0200

    Done with doctest of square shape printer

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+'''This module contains functions to print square shapes'''
+
+
+def print_square(size):
+    ''' Prints a square with size and the lenght of one side'''
+
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    elif size < 0:
+        raise ValueError('size must be >= 0')
+    print('\n'.join(['#' * size] * size))
