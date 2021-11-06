03731afecc6b0d060e390571934f61acb0cd1d54
PyBaker <freddygekko3@gmail.com>
2021-11-06 22:17:25 +0200

    added text indentation module with tests

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,61 @@
 #!/usr/bin/python3
+''' Module with funtions to manipulate text'''
+
+
+def text_indentation(text):
+    ''' Function to and new line after each occurance of "." "?" and ":"'''
+
+    if not isinstance(text ,str):
+        raise TypeError('text must be a string')
+
+
+    for char in '.?:':
+        text = text.replace(char, char + "\n")
+
+
+    #text = text.split(sep='\n')
+    #text = text.strip(' ')
+
+    # print (' '.join([line.strip("BAZZ")] for line in text))
+    print ('\n\n'.join([line.strip() for line in text.split('\n')]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    '''
+    for char in ".?:":
+        text = text.replace(char, char + "\n\n")
+    list_lines = [lines.strip(' ') for lines in text.split('\n')]
+    '''
