8fe57bd004780826cf92d3a18ba4d444063c81ec
PyBaker <freddygekko3@gmail.com>
2021-11-10 19:45:34 +0200

    added second square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" Module 10-Square
+    Contains a class Square with is a subclass of Rectangle
+    Order:
+        BaseGeometry --> Rectangle --> Square
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ Class for Square shapes """
+
+    def __init__(self, size):
+        """ initialises Square class """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
