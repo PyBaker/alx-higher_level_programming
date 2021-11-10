56bab783f951af22f8b1476801072f162b3171f5
PyBaker <freddygekko3@gmail.com>
2021-11-10 19:43:40 +0200

    code cleaning

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -13,5 +13,6 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """ initialises Square class """
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
