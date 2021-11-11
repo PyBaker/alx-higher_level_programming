061e4b8531173abaa6ce99541eb2f39aa1f2d3c1
PyBaker <freddygekko3@gmail.com>
2021-11-11 18:16:23 +0200

    Student class definition

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+""" Module ./9-student.py
+    Defines a Student class with fields and methods
+
+    Attributes:
+        first_name
+        second_name
+        age
+
+    Methods:
+        to_json(self) --> retrieves a dictionary representation of Student
+"""
+
+
+class Student:
+    """ Defines a student class """
+
+    def __init__(self, first_name, second_name, age):
+        """ initiallises student attributes """
+
+        self.first_name = first_name
+        self.second_name = second_name
+        self.age = age
+
+    def to_json(self):
+        """ returns dictionary representation of student """
+        return self.__dict__
