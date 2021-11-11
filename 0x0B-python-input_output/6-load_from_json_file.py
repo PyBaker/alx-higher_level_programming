8d731c2e1145b720b8542844c652e2465716c789
PyBaker <freddygekko3@gmail.com>
2021-11-11 15:25:40 +0200

    load JSON from obj

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" Module ./6-load_from_json_file.py
+    Contains:
+        Function load_from_json_file --> Creates python obj from JSON
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """ loads and create python obj from JSON """
+    with open(filename, "r", encoding="UTF-8") as f:
+        return json.load(f)
