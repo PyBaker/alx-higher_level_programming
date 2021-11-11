c2bed3a86a513f0d7cca41a312e7b0468b59224a
PyBaker <freddygekko3@gmail.com>
2021-11-11 15:09:05 +0200

    returns python representation of JSON string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" Module ./4-from_json_string.py
+    Contains:
+        Function from_json_string(obj) --> convert JSON to python Dictionary
+"""
+import json
+
+
+def from_json_string(my_str):
+    """ Converts JSON to python Dictionary """
+    return json.loads(my_str)
