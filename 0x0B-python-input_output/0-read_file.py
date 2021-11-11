0156be693cd211615fe6ede2f17f9ac15a4581b8
PyBaker <freddygekko3@gmail.com>
2021-11-11 14:14:34 +0200

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" Module 0-read_file
+
+    Contains:
+        Function - read_file(filename="") --> reads a text file
+"""
+
+
+def read_file(filename=""):
+    """ Reads a file """
+    with open(filename, "r") as f:
+        data = f.read()
+        print(data)
+
