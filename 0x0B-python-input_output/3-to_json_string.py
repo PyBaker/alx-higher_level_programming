f76bce92b243db9c7215f012425ea46bf7619f9f
PyBaker <freddygekko3@gmail.com>
2021-11-11 15:04:45 +0200

    returns JSON representation of a file

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" Module ./3-to_json_string.py
+    Contains:
+        Function to_json_string(obj) --> convert obj to json string
+"""
+import json
+
+def to_json_string(my_obj):
+    """ Converts object to JSON string """
+    return json.dumps(my_obj)
