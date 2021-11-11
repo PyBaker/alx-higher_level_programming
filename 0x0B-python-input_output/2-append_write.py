ebbd6ab435ab2619efeef4e0b9c52ef17968d98a
PyBaker <freddygekko3@gmail.com>
2021-11-11 14:52:32 +0200

    file to append text to file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" Modlule 2-append_write
+    Contains:
+        Function append_write(filename="", text="") --> appends to file
+"""
+
+
+def append_write(filename="", text=""):
+    """ Appends text to file """
+    with open(filename, mode="a", encoding="UTF-8") as f:
+        return(f.write(text))
