6ce5abafa360eb784e3b685c381a05b97789d6d4
PyBaker <freddygekko3@gmail.com>
2021-11-11 15:15:20 +0200

    writes JSON for python object to file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" Module ./5-save_to_json_file.py
+    Contains:
+        Function save_to_json_file --> writes obj to file in JSON format
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ Writes python data as JSON to file """
+    with open(filename, "w", ecoding="UTF-8") as f:
+        f.write(json.dumps(my_obj))
