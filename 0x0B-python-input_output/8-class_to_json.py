20589a715921c4d90551997caedd17d016f6a6e9
PyBaker <freddygekko3@gmail.com>
2021-11-11 16:52:49 +0200

    returns dixtinary description of class

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" Module ./8-class_to_json.py
+    Returns the dictionary description with simple data structure for JSON
+"""
+
+
+def class_to_json(obj):
+    """ Returns a dictionary description of a class"""
+    return obj.__dict__
