ea49d1337cda9279ed711a74809ed7ab0ac8876d
PyBaker <freddygekko3@gmail.com>
2021-11-11 14:42:38 +0200

    add file to write to file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+##!/usr/bin/python3
+""" Module 1-write_file:
+    Contains:
+        Function write_file() --> writes a string to a text file
+"""
+
+
+def write_file(filename="", text=""):
+    """ Writes a string to a text file """
+    with open(filename, mode="w", encoding="UTF-8") as f:
+        f.write(text)
