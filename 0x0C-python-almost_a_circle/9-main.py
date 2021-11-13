1fefac60fac971413961effa3678e0e51e02a682
PyBaker <freddygekko3@gmail.com>
2021-11-13 22:27:09 +0200

    hotfix

--- 0x0C-python-almost_a_circle/9-main.py
+++ 0x0C-python-almost_a_circle/9-main.py
@@ -2,7 +2,7 @@
 """ 9-main """
 from models.square import Square
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
 
     s1 = Square(5)
     print(s1)
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,13 +2,10 @@
 """ Module ./square.py
     Defines a square class
 """
-from rectangle import Rectangle
+from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """ Defines a Square """
     def __init__(self, size, x=0, y=0, id=None):
         """ initialises Square attributes """
-
-o = Square()
-print(o)
-
+        super().__init__(size, size, x, y, id)
