8409369ff41ab77d8574006c41fbc3210a009391
PyBaker <freddygekko3@gmail.com>
2021-11-14 01:40:37 +0200

    added json serializer

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """ Module ./base.py
     Initialises a Base Class
 """
+import json
 
 
 class Base:
@@ -9,8 +10,16 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ initialises Base class """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Returns the JSON representation of list """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
