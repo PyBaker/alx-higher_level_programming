221166e4d96e1834e9173c5313a4ba2b9fc064d6
PyBaker <freddygekko3@gmail.com>
2021-11-13 22:36:50 +0200

    overloaded __str__ magic method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,8 +4,14 @@
 """
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """ Defines a Square """
     def __init__(self, size, x=0, y=0, id=None):
         """ initialises Square attributes """
         super().__init__(size, size, x, y, id)
+
+    def __str__(self)
+        """ Return custom string """
+        return "[{:s}] ({:d}) {:d}/{:d} - {:d}"\
+                .format(self.__name__, self.id, self.x, self.y, self.size)
