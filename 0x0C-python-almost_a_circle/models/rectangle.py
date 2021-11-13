68f68d09a8fcb2f5e28525b92412a9b561f017e6
PyBaker <freddygekko3@gmail.com>
2021-11-13 04:10:38 +0200

    override the __str__ method of class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,6 +24,11 @@ class Rectangle(Base):
         """ Prints rectangle with '#' symbol """
         print("\n".join(["#" * self.__width] * self.__height))
 
+    def __str__(self):
+        return "[{:s}] ({:d}) {:d}/{:d} - {:d}/{:d} \
+                ".format(self.__class__.__name__, self.id,
+                         self.__x, self.__y, self.__width, self.height)
+
     @property
     def width(self):
         """ get width """
