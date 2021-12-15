87fbb347b97318a041ccb1b3e4808160deeb7679
PyBaker <freddygekko3@gmail.com>
2021-12-15 22:22:38 +0200

    hotfix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
+
 const result = Number(process.argv[2]);
 if (isNaN(result)) {
   console.log('Missing size');
 } else {
-  // let [i,j] = [0,0] // initialising counters
-
   for (let i = 0; i < result; i++) {
     console.log(('X').repeat(result));
   }
 }
-//  for(let i = 0;i < result;i++)
