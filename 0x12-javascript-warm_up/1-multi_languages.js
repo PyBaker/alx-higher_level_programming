0461f7c4990bb0b70644fa9c6afe853e1d857faf
PyBaker <freddygekko3@gmail.com>
2021-12-15 18:47:30 +0200

    hotfix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 // const myVar = "Javascript is amazing"
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
