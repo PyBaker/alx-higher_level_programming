408cf01770afe3b0c77b29e3b02813906ca153a8
PyBaker <freddygekko3@gmail.com>
2021-12-15 19:03:36 +0200

    hotfix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
 
+/* You could write the code as seen below
+
 if (process.argv[2]) {
   console.log(process.argv[2]);
 } else { console.log('No argument'); }
+
+*/
+
+console.log(process.argv[2] ? process.argv[2] : 'No argument');
