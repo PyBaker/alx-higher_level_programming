d3c0da79baf683756bba52bd2dde39006c17a831
PyBaker <freddygekko3@gmail.com>
2021-12-15 22:19:41 +0200

    used str.repeat() method

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-const result = parseInt(Number(process.argv[2]));
+const result = Number(process.argv[2]);
 if (isNaN(result)) {
   console.log('Missing size');
 } else {
+  // let [i,j] = [0,0] // initialising counters
+
   for (let i = 0; i < result; i++) {
-    let x = '';
-    for (let j = 0; j < result; j++) {
-      x += 'X';
-    }
-    console.log(x);
+    console.log(('X').repeat(result));
   }
 }
+//  for(let i = 0;i < result;i++)
