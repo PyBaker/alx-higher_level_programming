91c2fbe2e9d563fcd3b040528c3a6130c9b0e236
PyBaker <freddygekko3@gmail.com>
2021-12-21 10:51:05 +0200

    just some more practice in js

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,15 +1,7 @@
 #!/usr/bin/node
 
 // export default add = (a, b) => a + b; // function add(a, b)
-
-exports.add = (a, b) => a + b;
-exports.dummy = console.log('this is a dummy ting');
-exports.dummy2 = ('this is a socond ting');
-export default {
-    dummy3 = ()export modules  => {
-        return console.log('this is the third guy');
-    }
-};
-
+export let add = (a, b) => a + b;
+export let dummy2 = ('this is a socond ting');
 
 console.log(exports)
--- 0x12-javascript-warm_up/13-main.js
+++ 0x12-javascript-warm_up/13-main.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 
 const add = require('./13-add').add;
+const getme = require('./13-add').getme;
+console.log(getme);
+console.log(getme(4))
+
 console.log('this is the first:  ' + require('./13-main.js') + "          require('./13-main.js')");
 console.log('this is the second:  ' + add + "         add")
 console.log('this is the third:  ' + add() + "         add()")
