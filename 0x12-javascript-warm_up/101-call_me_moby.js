09a9e3d73251d3e2ff326feec32da27c675172c2
PyBaker <freddygekko3@gmail.com>
2021-12-21 14:48:50 +0200

    code cleaning

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
 exports.callMeMoby = (x, theFunction) => {
-  //just for control
+  // just for control
   let count = 0;
   while (count < x) {
     theFunction();
     count++;
   }
 };
-
