f7d55586f6ab2561639dac606c584aa4e811e2cc
PyBaker <freddygekko3@gmail.com>
2022-01-12 17:14:13 +0200

    hotfix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
-const esrever = (list) => list.reverse();
+const esrever = (list) => {
+  const newList = [];
+  for (let i = list.length; i > 0; i--) {
+    newList.push(list[i - 1]);
+  }
+  return newList;
+};
 module.exports.esrever = esrever;
