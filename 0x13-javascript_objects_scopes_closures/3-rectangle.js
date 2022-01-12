6c11cec7249c8639ebfba9ad461d2141f63fd551
PyBaker <freddygekko3@gmail.com>
2022-01-12 14:08:33 +0200

    added print method

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w && h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const count = [...Array(this.height).keys()];
+    // for (let i in count)
+    for (let i = 0; i < count.length; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
