93be3f6d8728b7570aa891eab925e355da32e943
PyBaker <freddygekko3@gmail.com>
2022-01-12 14:26:57 +0200

    added rotate and double method

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,33 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w && h) > 0) {
+      this.width = Number(w);
+      this.height = Number(h);
+    }
+  }
+
+  // prints shape using 'X' string
+  print () {
+    const count = [...Array(this.height).keys()];
+    // for (let i in count)
+    for (let i = 0; i < count.length; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  // rotates shape switching height and width
+  rotate () {
+    let temp = 0;
+    temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  // multiplies width and height of rectangle by 2
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+module.exports = Rectangle;
