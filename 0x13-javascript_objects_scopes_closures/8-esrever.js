b3f5db68dc9487c66e09ac31fa0021e4b331ba89
PyBaker <freddygekko3@gmail.com>
2022-01-12 20:04:31 +0200

    added function for logging

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const esrever = (list) => {
   const newList = [];
-  for (let i = list.length; i > 0; i--) {
-    newList.push(list[i - 1]);
+  for (let i = list.length; i >= 0; i--) {
+    newList.push(list[i]);
   }
   return newList;
 };
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+let counter = 0;
+const functionForLogging = function (argument) {
+  // or more simply console.log("%d: %s", counter, argument);
+  console.log(`${counter}: ${argument}`);
+  counter++;
+};
+module.exports.logMe = functionForLogging;
