d07de7d3bf37fb0f74d7a9ff1f63318d1065e1a1
PyBaker <freddygekko3@gmail.com>
2022-01-12 15:31:39 +0200

    added class method into Square class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  // prints squre with variable c
+  charPrint (c) {
+    super.print(c);
+  }
+}
+module.exports = Square;
