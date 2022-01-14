09e1b3e1db44f396f5b514c15c80546d5cfb6c9f
PyBaker <freddygekko3@gmail.com>
2022-01-15 00:31:28 +0200

    done tasks

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,1 @@
-#!/usr/bin/python3
-"""
-class definition of a State and an instance Base = declarative_base():
-"""
-
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-
-Base = declarative_base()
-
-
-class State(Base):
-    """State Class"""
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True, unique=True)
-    name = Column(String(128), nullable=False)
+#!/usr/bin/python
--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+// const dataFile = require('./101-data.js');j
+
+npm install lodash --save
+// console.log(dataFile.dict)
+const _ = require("lodash");
+let myDict = {
+  89: 1,
+  90: 2,
+  91: 1,
+  92: 3,
+  93: 1,
+  94: 2
+};
+
+const grouped = _.groupBy(myDict, Array(myDict).keys());
+console.log(grouped);
